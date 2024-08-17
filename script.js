c8347975fda4108c5ee7e8865105677cb22cf4d2
HIMANSHU DIWAKAR THAKRE <himanshuthakre7509@gmail.com>
2024-08-17 18:13:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let switchImg = document.getElementById("imgChange");
+
+function sumSea(){
+    switchImg.src = "images/sum.png";
+}
+
+function spgSea(){
+    switchImg.src = "images/rain.png";
+}
+
+function autSea(){
+    switchImg.src = "images/aut.png";
+}
+
+function winSea(){
+    switchImg.src = "images/win.png";
+}
